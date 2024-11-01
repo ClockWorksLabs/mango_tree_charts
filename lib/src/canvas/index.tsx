7e8848453851224782a72f98170ba373487d25fd
satwik singh <satwikk@satwiks-MacBook-Pro.local>
2024-11-01 14:56:02 +0530

    added gesture handler

--- lib/src/canvas/index.tsx
+++ lib/src/canvas/index.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import type { LayoutChangeEvent } from "react-native";
 import { Canvas } from "@shopify/react-native-skia";
+import { GestureDetector, Gesture, GestureHandlerRootView } from "react-native-gesture-handler";
+import { useSharedValue } from "react-native-reanimated";
 import { MasterContext } from "../context/MasterContext";
 import { Mesh } from "../components/mesh";
 
 const PlainCanvas = () => {
     const [canvasDimentions, setCanvasDimentions] = useState({ height: 0, width: 0 });
+    const scale = useSharedValue(1);
 
     const handleLayout = (event: LayoutChangeEvent) => {
         const dimention = event.nativeEvent.layout;
         setCanvasDimentions(dimention);
     }
 
+    const pinchGesture = Gesture.Pinch()
+        .onChange((event) => {
+            scale.value = event.scale;
+        })
+
     return (
-        <Canvas 
-            style={{ flex: 1 }}
-            onLayout={handleLayout}
-        >
-            <MasterContext.Provider value={canvasDimentions}>
-               <Mesh />
-            </MasterContext.Provider>
-        </Canvas>
+        <GestureHandlerRootView>
+            <GestureDetector gesture={pinchGesture}>
+            <Canvas 
+                style={{ flex: 1 }}
+                onLayout={handleLayout}
+            >
+                <MasterContext.Provider value={{ ...canvasDimentions, scale: scale.value }}>
+                    <Mesh />
+                </MasterContext.Provider>
+            </Canvas>
+        </GestureDetector>
+        </GestureHandlerRootView>
     );
 }
 
--- lib/src/context/MasterContext.tsx
+++ lib/src/context/MasterContext.tsx
@@ -3,9 +3,11 @@ import { createContext } from 'react';
 export interface CanvasDimensions {
     height: number;
     width: number;
+    scale: number;
 }
 
 export const MasterContext = createContext<CanvasDimensions>({
     height: 0,
-    width: 0
+    width: 0,
+    scale: 1
 }); 
